44cfe4977711d3af919959d41f627f2d285c9835
DESKTOP-RDU5TU8\WinDows <MetalTeam002@hotmail.com>
2025-12-20 22:52:25 +0200

    Fix Vercel proxy config: empty base URL and correct rewrite

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://3.76.5.172:6400' // Use Vercel proxy to call HTTP API
+  apiBaseUrl: '' // Use Vercel proxy to call HTTP API
 };
 
